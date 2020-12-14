036d98b9ff845aef5fdb1d6f62220ab87e81ba73
Markus van Kempen <markusvankempen43@gmail.com>
2020-12-14 13:21:22 -0500

    Create edp8266-dht11-AccessPoint.ino

---
+++ code/edp8266-dht11-AccessPoint.ino
@@ -0,0 +1,142 @@
+/*
+// mvk Access Point to Display Temperature and Humidity
+// mvk@ca.ibm.com for SNP / P-Tech
+
+//
+// Reference https://www.instructables.com/Interface-DHT11-Humidity-Sensor-Using-NodeMCU/
+//https://lastminuteengineers.com/esp8266-dht11-dht22-web-server-tutorial/
+*/
+#define  VERSION "14122020-1303"
+/* Create a WiFi access point and provide a web server on it. */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include "DHT.h"
+
+#define DHTPIN 4 // 4 = PIN D2   2 = PIN D4   // Digital pin connected to the DHT sensor
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+#ifndef APSSID
+#define APSSID "Markus-ESP8266"
+#define APPSK  "12345678" // has to be 8 char
+#endif
+
+/* Set these to your desired credentials. */
+const char *ssid = APSSID;
+const char *password = APPSK;
+
+ESP8266WebServer server(80);
+
+/* Just a little test message.  Go to http://192.168.4.1 in a web browser
+   connected to this access point to see it.
+*/
+String SendHTML(float TempCstat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600\" rel=\"stylesheet\">\n";
+  ptr +="<title>ESP8266 Weather Report</title>\n";
+  ptr +="<style>html { font-family: 'Open Sans', sans-serif; display: block; margin: 0px auto; text-align: center;color: #333333;}\n";
+  ptr +="body{margin-top: 50px;}\n";
+  ptr +="h1 {margin: 50px auto 30px;}\n";
+  ptr +=".side-by-side{display: inline-block;vertical-align: middle;position: relative;}\n";
+  ptr +=".humidity-icon{background-color: #3498db;width: 30px;height: 30px;border-radius: 50%;line-height: 36px;}\n";
+  ptr +=".humidity-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".humidity{font-weight: 300;font-size: 60px;color: #3498db;}\n";
+  ptr +=".temperature-icon{background-color: #f39c12;width: 30px;height: 30px;border-radius: 50%;line-height: 40px;}\n";
+  ptr +=".temperature-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".temperature{font-weight: 300;font-size: 60px;color: #f39c12;}\n";
+  ptr +=".superscript{font-size: 17px;font-weight: 600;position: absolute;right: -20px;top: 15px;}\n";
+  ptr +=".data{padding: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  
+   ptr +="<div id=\"webpage\">\n";
+   
+   ptr +="<h1>ESP8266 Weather Report</h1>\n";
+   ptr +="<div class=\"data\">\n";
+   ptr +="<div class=\"side-by-side temperature-icon\">\n";
+   ptr +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
+   ptr +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
+   ptr +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
+   ptr +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
+   ptr +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
+   ptr +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
+   ptr +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"/>\n";
+   ptr +="</svg>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"side-by-side temperature-text\">Temperature</div>\n";
+   ptr +="<div class=\"side-by-side temperature\">";
+   ptr +=(int)TempCstat;
+   ptr +="<span class=\"superscript\">C</span></div>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"data\">\n";
+   ptr +="<div class=\"side-by-side humidity-icon\">\n";
+   ptr +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n\"; width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
+   ptr +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
+   ptr +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
+   ptr +="</svg>\n";
+   ptr +="</div>\n";
+   ptr +="<div class=\"side-by-side humidity-text\">Humidity</div>\n";
+   ptr +="<div class=\"side-by-side humidity\">";
+   ptr +=(int)Humiditystat;
+   ptr +="<span class=\"superscript\">%</span></div>\n";
+   ptr +="</div>\n";
+
+  ptr +="</div>\n";
+  ptr +="<br><br><button onclick=window.location.reload();> Refresh </button>";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+void handleRoot() {
+  Serial.println("handleRoot");
+
+    // Read temperature as Celsius (the default)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  String myt = "<html><h1>You are connected<br><br><br>Stations connected to soft-AP = "+ (String)ssid ;
+  myt = myt+"<br>Temperature = "+t+"<br>Humidity = "+h;
+  myt += "<br><br><button onclick=window.location.reload();> Refresh </button>";
+  myt = myt+"</html>";
+  
+  server.send(200, "text/html",  SendHTML(t,h));
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Markus - Configuring access point...");
+  Serial.println(VERSION);
+    Serial.println(APSSID);
+  Serial.println(APPSK);
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+    Serial.println(" dht.begin()");
+  dht.begin();
+  delay(1000);
+  
+}
+
+void loop() {
+  // Serial.print(" loop");
+  server.handleClient();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+ // Serial.println("readTemperature="+String(t));
+ // Serial.println("readHumidity="+String(h));
+}
