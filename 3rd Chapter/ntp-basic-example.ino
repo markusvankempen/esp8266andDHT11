530cc8199ea7b194b6eeaadb5adaf42a16ab0add
Markus van Kempen <markusvankempen43@gmail.com>
2020-12-16 15:12:19 -0500

    Create ntp-basic-example.ino

---
+++ 3rd Chapter/ntp-basic-example.ino
@@ -0,0 +1,48 @@
+
+
+#include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+//#include <WiFi.h> // for WiFi shield
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
+
+const char *ssid     = "";
+const char *password = "";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -18000, 60000); /// EST
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+
+  Serial.println(timeClient.getFormattedTime());
+
+  delay(1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
